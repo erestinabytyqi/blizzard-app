332f526cfacb34c069a5697950cadd62e47de6c7
Erestine Bytyqi <erestinabytyqi49282@gmail.com>
2024-06-13 16:17:26 +0200

    updated our solutions

--- src/components/OurSolutions.js
+++ src/components/OurSolutions.js
@@ -3,14 +3,14 @@ import Solutions from './Solutions'
 
 function OurSolutions() {
   return (
-    <div className='text-white'>   
-        <h1>Our Solutions</h1>
-        <p>
-            We deliver cutting-edge technology for networking and connectivity, 
+    <div className='text-white mt-40'>   
+        <h1 className='text-4xl mt-10 font-bold ml-20'>Our Solutions</h1>
+        <p className='text-xl text-gray-400 ml-20 pt-5'>
+            We deliver cutting-edge technology for networking and connectivity,<br/> 
             ensuring your peace of mind with unparalleled security and reliability
 
         </p>
-        <div>
+        <div className='mt-10'>
             <Solutions/>
         </div>
     </div>
